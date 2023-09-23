dd3f49bc1af0975212af36e46d17292a5fca15d8
Patitotective <cristobalriaga@gmail.com>
2023-09-23 18:23:26 -0500

    Update opengl.nim

--- src/nimgl/opengl.nim
+++ src/nimgl/opengl.nim
@@ -1,7 +1,7 @@
 # Written by Leonardo Mariscal <leo@ldmd.mx>, 2019
 
 ## Modern OpenGL Bindings
-## ====
+## ======================
 ## WARNING: This is a generated file. Do not edit
 ## Any edits will be overwritten by the generator.
 ##
